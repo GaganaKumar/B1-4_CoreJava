687aa2337fc5cef0b3de0cc37bc3f35aec22cdbe
ADMIN <ADMIN@ADMIN-PC>
2022-04-04 16:34:02 +0530

    first commit

---
+++ Day4-Gagana/src/package1/example1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class example1 {
+
+	public static void main(String[] args) 
+		{
+			System.out.println("welcome to concept of packages");
+		}
+	}
+
+
+
---
+++ Day4-Gagana/src/package3/sbi.java
@@ -0,0 +1,24 @@
+package package3;
+
+public class sbi 
+{
+	public int balance;
+	public sbi(int bal)
+	{
+		balance=bal;
+	}
+	
+	public void display()
+	{
+		System.out.println(balance);
+	}
+
+	public static void main(String[] args)
+	{
+		sbi s=new sbi(50800);
+		s.display();
+
+
+	}
+
+}
