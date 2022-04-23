4b5ab9184b2df87102657a76d4caedbc40df7cb4
user <user@DESKTOP-2NT39JA.alphionsee.in>
2022-04-24 00:59:51 +0530

    commit

---
+++ Day14-Strings/src/strings/Example2.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class Example2 {
+
+	public static void main(String[] args) 
+	{
+		String str1=new String("hello");
+		String str2=new String("us");
+
+
+		System.out.println(str1==str2);
+		System.out.println(str1.equals(str2));
+
+	}
+
+}
