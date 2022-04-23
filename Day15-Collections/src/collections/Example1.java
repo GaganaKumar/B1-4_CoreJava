e6947a0116b6ea080d218cbd5c9708bae90543b3
user <user@DESKTOP-2NT39JA.alphionsee.in>
2022-04-24 00:55:03 +0530

    commit

---
+++ Day15-Collections/src/collections/Example1.java
@@ -0,0 +1,16 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class Example1 {
+
+	public static void main(String[] args)
+	{
+		ArrayList obj=new ArrayList<>();//we can declare array size in ()
+		obj.add(10.0);
+		obj.add("john");
+		System.out.println(obj);
+
+	}
+
+}
---
+++ Day15-Collections/src/collections/Example3.java
@@ -0,0 +1,29 @@
+package collections;
+
+import java.util.LinkedList;
+
+public class Example3 {
+
+	public static void main(String[] args)
+	{
+		LinkedList obj=new LinkedList<>();
+		obj.add(10);
+		obj.add("jonny");
+		obj.add(2.0);
+		obj.add(9);
+		System.out.println(obj);
+		obj.addFirst(5);
+		obj.addLast(25);
+		System.out.println(obj);
+		System.out.println(obj.size());
+		@SuppressWarnings("rawtypes")
+		LinkedList obj1=new LinkedList<>();
+		obj1.add("joe");
+		obj1.add("john");
+		obj1.add("jesh");
+		obj.addAll(2,obj1);
+		System.out.println(obj);
+
+	}
+
+}
