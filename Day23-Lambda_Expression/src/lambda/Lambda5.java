c7655a84a2c862482f524fe1b7a9760744261d6c
user <user@DESKTOP-2NT39JA.alphionsee.in>
2022-05-04 16:07:55 +0530

    commit

---
+++ Day23-Lambda_Expression/src/lambda/Lambda5.java
@@ -0,0 +1,22 @@
+package lambda;
+
+
+interface C
+{
+	String print(String message);
+}
+
+public class Lambda5 {
+
+	public static void main(String[] args) {
+		//lambda expression with return
+		
+		C obj=(String message)->
+		{
+			return message;
+			//System.out.println("the name is "+message);
+		};
+		System.out.println("the name is: "+obj.print(" john"));
+		}
+
+}
