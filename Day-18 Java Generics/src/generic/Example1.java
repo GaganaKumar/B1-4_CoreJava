ddd9ba4999837e265edbb746859826691f507f45
user <user@DESKTOP-2NT39JA.alphionsee.in>
2022-05-04 15:30:48 +0530

    commit

---
+++ Day-18 Java Generics/src/generic/Example1.java
@@ -0,0 +1,23 @@
+package generic;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("gen");
+		a.add("genny");
+		String s=a.get(1);
+		System.out.println(s);
+		Iterator<String> itr=a.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+	}
+}
+
+
