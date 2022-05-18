57442b183bf30f2910ee77e563c1bb5bb6bba288
user <user@DESKTOP-2NT39JA.alphionsee.in>
2022-05-18 23:55:45 +0530

    commit

---
+++ Bank Application/src/framework/CurrentAcc.java
@@ -0,0 +1,23 @@
+package framework;
+
+public abstract class CurrentAcc extends BankAcc 
+{
+	private final float MINBAL;
+
+	public CurrentAcc(int accNo, String accNm, float accBal,float MINBAL ) 
+	{
+		super(accNo,accNm,accBal);
+		this.MINBAL=MINBAL;
+	}
+	
+	public void withdraw(float accBal) {
+		System.out.println("Account no: "+this.getAccNo()+"\n "+" Account name: "+this.getAccNm()+ "\n  "+"Account Balance: "+(accBal+MINBAL));
+	}
+
+
+	@Override
+	public String toString() {
+		return String.format("CurrentAcc [MINBAL=%s, toString()=%s]", MINBAL,super.toString());
+	}
+
+}
---
+++ Bank Application/src/framework/SavingAcc.java
@@ -0,0 +1,29 @@
+package framework;
+
+public abstract class SavingAcc extends BankAcc 
+{
+	private  boolean isSalaried;
+	static final private float MINBAL = 500;
+
+	public SavingAcc(int accNo, String accNm, float accBal, boolean isSalaried) {
+		super(accNo, accNm, accBal);
+		this.isSalaried=isSalaried;
+		
+	}
+	public void withdraw(float accBal) {
+		System.out.println("Account no: "+this.getAccNo()+"\n "+" Account name: "+this.getAccNm()+ "\n  "+"Account Balance: "+accBal);
+	}
+
+
+	
+
+	@Override
+	public String toString() {
+		return String.format("SavingAcc [isSalaried=%s, toString()=%s]", isSalaried,
+				super.toString());
+	}
+	public static float getMinbal() {
+		return MINBAL;
+	}
+
+}
